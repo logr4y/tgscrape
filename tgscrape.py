cf380b896fa5f282cf812e7d63ffbf3b5d7b59f1
logray <logr4y@users.noreply.github.com>
2018-01-13 16:05:46 -0800

    fixed a couple of bugs, cleaned code up

--- tgscrape.py
+++ tgscrape.py
@@ -1,60 +1,83 @@
 #!/usr/bin/env python3
-#
-# Quick and dirty public Telegram group message scraper
-#
-# Usage:
-#     python3 tgscrape.py <groupname> [minid] [maxid]
-#
-# Example:
-#     python3 tgscrape.py fun_with_friends 1 1000
-#       - dumps messages 1 through 1000 from the group @fun_with_friends
-#
-# The loop stops when it finds 20 consecutive empty messages
+# -*- coding: utf-8 -*-
+"""
+Quick and dirty public Telegram group message scraper
 
-import requests
+Usage:
+    $ python3 tgscrape.py <groupname> [minid] [maxid]
+
+Example:
+    $ python3 tgscrape.py fun_with_friends 1 1000
+        - dumps messages 1 through 1000 from the group @fun_with_friends
+
+The loop stops when it finds 20 consecutive empty messages
+"""
+
 import sys
+import time
+import requests
 from bs4 import BeautifulSoup
 
-def usage(_bin):
-    print('Usage: {} <groupname> [minid] [maxid]'.format(_bin))
+def usage(pyfile):
+    """ Usage """
+    print('Usage: {} <groupname> [minid] [maxid]'.format(pyfile))
 
-_argnum = len(sys.argv)
+def get_sender(obj):
+    """ Retrieves the sender of a message """
+    obj = obj.find('div', class_='tgme_widget_message_author')
+    author = list(obj.children)[0]
+    return_name = author.text
+    if author.name == 'a':
+        return_username = author['href'].split('/')[3]
+    elif author.name == 'span':
+        return_username = ''
+    else:
+        print("author retrieve error")
+        exit()
+    return (return_name, return_username)
 
-if _argnum < 2:
+argnum = len(sys.argv)
+
+if argnum < 2:
     usage(sys.argv[0])
     exit()
 
-_minid = 0
-_maxid = -1
-_groupname = sys.argv[1]
-_max404 = 20
+min_id = int(sys.argv[2]) if argnum >= 3 else 0
+max_id = int(sys.argv[3]) if argnum >= 4 else -1
+groupname = sys.argv[1]
+max_404 = 20
 
-if _argnum >= 3:
-    _minid = int(sys.argv[2])
-    if _argnum >= 4:
-        _maxid = int(sys.argv[3])
+url = 'https://t.me/{}/'.format(groupname)
 
-_url = 'https://t.me/{}/'.format(_groupname)
-
-_id = _minid
-_cnt404 = 0
+msg_id = min_id
+cnt_404 = 0
 while True:
-    _rurl = _url + str(_id) + '?embed=1'
-    _output = requests.get(_rurl)
-    if _output.status_code < 400:
-        _soup = BeautifulSoup(_output.text, 'html.parser')
-        _msg = _soup.find('div', class_='tgme_widget_message_text')
-        if _msg:
-            _author = _soup.find('a', class_='tgme_widget_message_author_name')
-            _username = _author['href'].split('/')[3] if _author['href'] else ''
-            _datetime = _soup.find('time')['datetime']
-            print('[{}] {} (@{}): {}'.format(_datetime, _author.text, _username, _msg.text))
+    r_url = url + str(msg_id) + '?embed=1'
+    response = requests.get(r_url)
+    if response.status_code < 400:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        msg = soup.find_all('div', class_='tgme_widget_message_text')
+        if msg:
+            if len(msg) == 2:
+                quote = msg[0].text
+                msg = msg[1]
+            else:
+                msg = msg[0]
+                quote = ''
+            (name, username) = get_sender(soup)
+            datetime = soup.find('time')['datetime']
+            print(
+                '[{}] {}{} {}{}'.format(datetime,
+                                      name,
+                                      ' (@{}):'.format(username) if username else ':',
+                                      '{{ {} }} '.format(quote) if quote else '',
+                                      msg.text))
     else:
-        _cnt404 += 1
-        if _cnt404 == _max404:
+        cnt_404 += 1
+        if cnt_404 == max_404:
             exit()
 
-    if _maxid > 0 and _id == _maxid:
+    if msg_id == max_id:
         exit()
-    _id += 1
-
+    msg_id += 1
+    time.sleep(0.5)
